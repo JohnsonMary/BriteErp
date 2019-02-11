e281ff898fcb80cd1b72e2f07f7ef4990f8b0657
avsararas <45132030+avsararas@users.noreply.github.com>
2019-02-11 10:44:59 -0800

    Update ConfSalesChannelsPage.java

--- src/test/java/com/briteErp/pages/ConfSalesChannelsPage.java
+++ src/test/java/com/briteErp/pages/ConfSalesChannelsPage.java
@@ -1,6 +1,8 @@
 package com.briteErp.pages;
 
 import com.briteErp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ConfSalesChannelsPage {
@@ -8,5 +10,22 @@ public class ConfSalesChannelsPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy(linkText = "Sales")
+    public WebElement salesLink;
 
+    @FindBy(linkText = "SalesChannel")
+    public WebElement salesChannel;
+
+    @FindBy(xpath = "//button[contains(text(),'Create')]")
+    public WebElement createButton;
+
+    @FindBy(xpath = "//input[@id='o_field_input_2871']")
+    public WebElement salesChannelBox ;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/button[1]")
+    public WebElement saveButton ;
+
+
+    @FindBy (linkText = "active")
+    public WebElement activeLink ;
 }
